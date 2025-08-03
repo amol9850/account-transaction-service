f43429875ce534db4665c329416f08d3d23d21da
Amol Dhanure <amoldhanure@amols-macbook-air.local>
2025-08-03 15:23:42 +0530

    Remove target and .idea folders from git tracking and add .gitignore

--- target/classes/data.sql
+++
@@ -1,6 +0,0 @@
--- Insert Operation Types
-INSERT INTO operation_types (operation_type_id, description) VALUES
-(1, 'CASH PURCHASE'),
-(2, 'INSTALLMENT PURCHASE'),
-(3, 'WITHDRAWAL'),
-(4, 'PAYMENT');
